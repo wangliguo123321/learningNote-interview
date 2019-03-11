ccf9b62c0eba3b33153aa32c6550a9cc3fc177a2
可以叫我利利 <2223686658@qq.com>
2019-03-11 19:50:25 +0800

    补充Redis的RDB 和AOF相关

--- learningNote/Algorithm/util/Main.java
+++ learningNote/Algorithm/util/Main.java
@@ -1,9 +1,19 @@
+import java.util.Scanner;
+
 /**
  * @Author liguo
  * @Description
  */
 public class Main {
     public static void main(String[] args) {
+            Scanner in = new Scanner(System.in);
+            long  n = in.nextInt();
+            long m= in.nextInt() ;
+            long answer= 0;
+            if(n==0&&m==0)        answer = 0;
 
+            else if(n>=m)        answer =1 ;
+            else if (m >n)        answer =m%n+1;
+            System.out.println(answer);
     }
 }
